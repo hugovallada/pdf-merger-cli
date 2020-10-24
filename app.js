1299434bd2b0b4dbfe44e055925eedb6b95bf315
Hugo Vallada <valladahugo@gmail.com>
2020-10-24 15:00:13 -0300

    Adicionada opçao de nao colocar o safety

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-import path from 'path';
-import os from 'os';
-import walk from 'walk';
-import inquirer from 'inquirer';
+#! /usr/bin/node
+
 import merge from 'easy-pdf-merge';
+import inquirer from 'inquirer';
+import walk from 'walk';
 
 inquirer.prompt([
   {
     name: "mesclar",
     type: "confirm",
-    message: "Deseja mesclar?"
+    message: "Deseja adicionar a tag de merged ao nome do arquivo?"
   },
   {
     name: "safety",
     type: "list",
-    message: "Qual o tipo de safety vc quer?",
+    message: "Qual o tipo de safety você deseja utilizar?\n(Selecione qualquer opção caso tenha selecionado Não na opção anterior)",
     choices: ["Merge", "_merged", "merged"]
   },
   {
@@ -27,38 +27,33 @@ inquirer.prompt([
     message: "Qual o nome do novo arquivo?",
   }
 ]).then(answer => {
-  if (answer.mesclar) {
-    console.log("Início da Mescla do PDF!!!")
 
-    let source = [];
+  let source = [];
 
-    const walker = walk.walk(answer.caminho, {
-      followLinks: false,
-      filters: ["Merge", "merged", "_merged"]
-    });
+  const walker = walk.walk(answer.caminho, {
+    followLinks: false,
+    filters: ["Merge", "merged", "_merged"]
+  });
 
-    walker.on("files", (root, fileStat, next) => {
-      fileStat.forEach(file => {
-        if (file.name.includes('.pdf')) {
-          if ((file.name.includes('Merge') || (file.name.includes('merged')) || (file.name.includes('_merged')))) {
-            return false;
-          } else {
-            source.push(`${answer.caminho}${file.name}`);
-          }
-        }
-
-      })
-
-      merge(source, `${answer.caminho}${answer.novo}${answer.safety}.pdf`, (err) => {
-        if (err) {
-          return "success";
+  walker.on("files", (root, fileStat, next) => {
+    fileStat.forEach(file => {
+      if (file.name.includes('.pdf')) {
+        if ((file.name.includes('Merge') || (file.name.includes('merged')) || (file.name.includes('_merged')))) {
+          return false;
         } else {
-          return "fail";
+          source.push(`${answer.caminho}${file.name}`);
         }
-      });
+      }
+
+    })
+
+    merge(source, `${answer.caminho}${answer.novo}${answer.mesclar ? answer.safety : ''}.pdf`, (err) => {
+      if (err) {
+        return "success";
+      } else {
+        return "fail";
+      }
     });
-
-
-
-  }
+  });
+  
 }).catch(err => console.log("Algo aconteceu"))
